362e76f957a31d71dc77bae60ae4dbc187994b77
António Barbosa <1190404@isep.ipp.pt>
2024-10-15 21:57:16 +0100

    feature/cogsi-13 #13: added base project

---
+++ CS02/tut-rest-gradle/app/src/main/test/payroll/EmployeeTest.java
@@ -0,0 +1,31 @@
+package payroll;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmployeeTest {
+
+    @Test
+    public void testEmployee() {
+        Employee emp = new Employee("name", "role" );
+        emp.setJobYears(5);
+        Employee emp2 = new Employee("name", "role", 5);
+        assertEquals(emp, emp2);
+    }
+
+    @Test
+    public void testEmployee2() {
+        Employee emp = new Employee("name", "role",5);
+        emp.setEmail("1240000@isep.ipp.pt");
+        Employee emp2 = new Employee("name", "role", 5,"1240000@isep.ipp.pt");
+        assertEquals(emp, emp2);
+    }
+
+    @Test
+    public void setEmail(){
+        Employee emp2 = new Employee("name", "role", 5,"1240000@isep.ipp.pt");
+        String email = "1240000@isep.ipp.pt";
+        assertEquals(emp2.getEmail(),email);
+    }
+}
