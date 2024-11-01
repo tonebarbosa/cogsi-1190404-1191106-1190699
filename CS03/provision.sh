05a1f7f08fe35b20a4c6fd329736e2663f269bb4
Antonio Barbosa (CTW) <antonio.barbosa@ctw.bmwgroup.com>
2024-11-01 22:39:29 +0000

    feature/cogsi-18: add vagrant file and provisioning

---
+++ CS03/provision.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Starting provisioning..."
+sudo apt-get update
+sudo apt-get install -y git openjdk-11-jdk maven gradle
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export PATH=$PATH:/opt/gradle/bin
+
+if [ ! -d "/home/vagrant/cs03" ]; then
+  git clone https://github.com/tonebarbosa/cogsi-1190404-1191106-1190699.git /home/vagrant/cs03
+fi
+
+cd /home/vagrant/cs03
+
+./mvnw clean install -DskipTests
+./gradlew build
+
+nohup java -jar target/*.jar &
